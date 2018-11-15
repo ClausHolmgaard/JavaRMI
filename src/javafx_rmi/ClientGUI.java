3074b8e60046e55ed2a70280935f2fc58000d22d
Claus <github@clausnet.dk>
2018-11-15 11:17:51 +0100

    client update

--- src/javafx_rmi/ClientGUI.java
+++ src/javafx_rmi/ClientGUI.java
@@ -52,6 +52,8 @@ public class ClientGUI extends JPanel{
         frame = new JFrame("GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
+        frame.getContentPane().setSize(300, 300);
         //frame pack means the program GUI can be scaleable
         frame.pack();
         frame.setVisible(true);
--- src/javafx_rmi/Main.java
+++ src/javafx_rmi/Main.java
@@ -4,22 +4,12 @@ public class Main {
 
     static int port = 2002;
     static String rmiObjectName = "rmi://" + "localhost:" + port + "/Shared";
-    //static String rmiObjectName = "//localhost/shared";
 
     public static void main(String[] args) {
         System.out.println(("Starting server..."));
         RMIServer s = new RMIServer(port, rmiObjectName);
 
-        try
-        {
-            Thread.sleep(100);
-        }
-        catch(InterruptedException ex)
-        {
-            Thread.currentThread().interrupt();
-        }
-
         System.out.println("Starting client...");
-        RMIClient c = new RMIClient(port, rmiObjectName);
+        RMIClient c = new RMIClient();
     }
 }
