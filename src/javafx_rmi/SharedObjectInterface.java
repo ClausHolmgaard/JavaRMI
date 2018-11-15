48bbcfd2162150e3ccdf2bd1a2eb8744f6676728
Claus <github@clausnet.dk>
2018-11-15 10:53:51 +0100

    First backend

--- src/javafx_rmi/SharedObjectInterface.java
+++ src/javafx_rmi/SharedObjectInterface.java
@@ -7,4 +7,9 @@ import java.rmi.RemoteException;
 public interface SharedObjectInterface extends Remote {
     void setString(String ind) throws RemoteException;
     String getString() throws  RemoteException;
+
+    void setInfo(InfoClass i) throws  RemoteException;
+    InfoClass getInfo() throws RemoteException;
+
+    int getHash() throws RemoteException;
 }
